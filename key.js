be3a5410bcdbd31fa924f7816533d93721a6fdf4
replitprivet-dotcom <replitprivet@gmail.com>
2026-09-23 05:36:04 +0530

    Update key.js

--- key.js
+++ key.js
@@ -1,1 +1,20 @@
-
+{
+  "created_at": "2026-09-23T05:32:00+05:30",
+  "api_keys": [
+    {
+      "api_key": "rb_live_abc123",
+      "active": true,
+      "expiry_days": 1
+    },
+    {
+      "api_key": "rb_live_def456",
+      "active": true,
+      "expiry_days": 7
+    },
+    {
+      "api_key": "rb_live_ghi789",
+      "active": false,
+      "expiry_days": 30
+    }
+  ]
+}
